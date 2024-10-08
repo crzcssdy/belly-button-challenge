2de8149f7a91569a548b1fd4407daf9bc5e2d88d
crzcssdy <cassidy.ccruz@gmail.com>
2024-10-07 22:12:18 -0700

    Finalized the code to deploy the visualizations

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -27,29 +27,69 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    let samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+    let resultsArray = samples.filter(sampleObj => sampleObj.id === sample);
+    let result = resultsArray[0];
 
     // Get the otu_ids, otu_labels, and sample_values
+    let otuIds = result.otu_ids;
+    let otuLabels = result.otu_labels;
+    let sampleValues = result.sample_values;
 
+    // console.log("OTU IDs:", otuIds);
+    //console.log("OTU Labels:", otuLabels);
+    //console.log("Sample Values:", sampleValues);
 
     // Build a Bubble Chart
+    let bubbleTrace = {
+      x: otuIds,
+      y: sampleValues,
+      text: otuLabels,
+      mode: 'markers',
+      marker: {
+        size: sampleValues,
+        color: otuIds,
+        colorscale: 'red'
+      }
+    };
 
+    const bubbleData = [bubbleTrace];
+
+    let bubbleLayout = {
+      title: 'Bacteria Cultures per Sample',
+      xaxis: { title: 'OTU ID' },
+      yaxis: { title: 'Sample Values' },
+      hovermode: 'closest'
+    };
 
     // Render the Bubble Chart
-
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    let yticks = otuIds.slice(0,10).map(id => `OTU ${id}`);
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
+    let barTrace = {
+      x: sampleValues.slice(0,10).reverse(),
+      y: yticks.reverse(),
+      text: otuLabels.slice(0,10).reverse(),
+      type: 'bar',
+      orientation: 'h'
+    };
+    let barData = [barTrace];
+    let barLayout = {
+      title: 'Top 10 Bacteria Cultures Found',
+      xaxis: { title: 'Sample Values' },
+      yaxis: { title: 'OTU ID' }
+    };
 
-
+    // console.log("Bar Chart Data:", barData);
+    
     // Render the Bar Chart
-
+    Plotly.newPlot('bar', barData, barLayout);
   });
 }
 
@@ -58,28 +98,32 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-
+    let sampleNames = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
+    let dropdown = d3.select("#selDataset");
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+    sampleNames.forEach((sample) => {
+      dropdown.append("option").text(sample).property("value", sample);
+    });
 
     // Get the first sample from the list
-
+    const firstSample = sampleNames[0];
 
     // Build charts and metadata panel with the first sample
-
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildCharts(newSample);
+  buildMetadata(newSample);
 }
 
 // Initialize the dashboard
